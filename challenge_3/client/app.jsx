93a28bbc057d7dd968203a2b6119f1b6810c6ef2
Kelly Willard <kellywillard@Kellys-MacBook-Pro.local>
2019-11-20 15:59:07 -0800

    Construct forms

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,86 @@
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      level: 0,
+    }
+    this.userInfo = {};
+
+    this.onCheckoutClick = this.onCheckoutClick.bind(this)
+    this.onCreateAccountClick = this.onCreateAccountClick.bind(this)
+    this.onShippingClick = this.onShippingClick.bind(this)
+ }
+
+ onCheckoutClick(e) {
+   e.preventDefault();
+   this.setState({
+     level: 1
+   })
+ }
+
+ onCreateAccountClick(e) {
+  e.preventDefault();
+   this.setState({
+     level: 2
+   })
+ }
+
+ onShippingClick(e) {
+   e.preventDefault();
+   this.setState({
+     level: 3
+   })
+ }
+
+ render() {
+   if (this.state.level === 0) {
+     return(
+       <button onClick={this.onCheckoutClick}>Checkout Now</button>
+     )
+   } 
+   else if(this.state.level === 1) {
+     return (
+       <form>
+         <div>Name: <input type="text" name="name"></input></div><br />
+         <div>Email: <input type="text" name="email"></input></div><br />
+         <div>Password: <input type="password" name="password"></input></div><br />
+         <button onClick={this.onCreateAccountClick}>Create Account</button>
+       </form>
+      )
+    } 
+   else if (this.state.level === 2) {
+     return (
+     <form>
+       <div>Shipping Information:</div> 
+       <div>Address: <input type="text" placeholder="Line 1" name="address1"></input></div>
+       <div>Address: <input type="text" placeholder="Line 2" name="address2"></input></div>
+       <div>City: <input type="text" placeholder="City" name="city"></input></div>
+       <div>State: <input type="text" placeholder="State" name="state"></input></div>
+       <div>Zip Code: <input type="number" placeholder="Zip Code" name="zip"></input></div>
+       <div>Phone Number: <input type="number" placeholder="Phone Number" name="phone"></input></div>
+       <button onClick={this.onShippingClick}>Submit Shipping Info</button>
+     </form>
+     )
+   } else if (this.state.level === 3) {
+     return (
+       <form>
+      <div>Payment Information:</div> 
+      <div>Credit Card Number: <input type="number" placeholder="Credit Card Number" name="cardnumber"></input></div>
+      <div>Expiration Date: <input type="date" placeholder="Exp Date" name="expdate"></input></div>
+      <div>CVV: <input type="number" placeholder="CVV" name="cvv"></input></div>
+      <div>Zip Code: <input type="number" placeholder="Zip Code" name="billzip"></input></div>
+      <button onClick={this.onShippingClick}>Submit Payment Info</button>
+      </form>
+     )
+   }
+ }
+}
 
 
+
+ReactDOM.render(<App />, document.getElementById("app"))
+
+// ReactDOM.render(
+//     <h1>Hello, world!</h1>,
+//     document.getElementById('app')
+//   );
---
+++ challenge_3/public/compiled.js
@@ -0,0 +1,108 @@
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      level: 0
+    };
+    this.userInfo = {};
+    this.onCheckoutClick = this.onCheckoutClick.bind(this);
+    this.onCreateAccountClick = this.onCreateAccountClick.bind(this);
+    this.onShippingClick = this.onShippingClick.bind(this);
+  }
+
+  onCheckoutClick(e) {
+    e.preventDefault();
+    this.setState({
+      level: 1
+    });
+  }
+
+  onCreateAccountClick(e) {
+    e.preventDefault();
+    this.setState({
+      level: 2
+    });
+  }
+
+  onShippingClick(e) {
+    e.preventDefault();
+    this.setState({
+      level: 3
+    });
+  }
+
+  render() {
+    if (this.state.level === 0) {
+      return React.createElement("button", {
+        onClick: this.onCheckoutClick
+      }, "Checkout Now");
+    } else if (this.state.level === 1) {
+      return React.createElement("form", null, React.createElement("div", null, "Name: ", React.createElement("input", {
+        type: "text",
+        name: "name"
+      })), React.createElement("br", null), React.createElement("div", null, "Email: ", React.createElement("input", {
+        type: "text",
+        name: "email"
+      })), React.createElement("br", null), React.createElement("div", null, "Password: ", React.createElement("input", {
+        type: "password",
+        name: "password"
+      })), React.createElement("br", null), React.createElement("button", {
+        onClick: this.onCreateAccountClick
+      }, "Create Account"));
+    } else if (this.state.level === 2) {
+      return React.createElement("form", null, React.createElement("div", null, "Shipping Information:"), React.createElement("div", null, "Address: ", React.createElement("input", {
+        type: "text",
+        placeholder: "Line 1",
+        name: "address1"
+      })), React.createElement("div", null, "Address: ", React.createElement("input", {
+        type: "text",
+        placeholder: "Line 2",
+        name: "address2"
+      })), React.createElement("div", null, "City: ", React.createElement("input", {
+        type: "text",
+        placeholder: "City",
+        name: "city"
+      })), React.createElement("div", null, "State: ", React.createElement("input", {
+        type: "text",
+        placeholder: "State",
+        name: "state"
+      })), React.createElement("div", null, "Zip Code: ", React.createElement("input", {
+        type: "number",
+        placeholder: "Zip Code",
+        name: "zip"
+      })), React.createElement("div", null, "Phone Number: ", React.createElement("input", {
+        type: "number",
+        placeholder: "Phone Number",
+        name: "phone"
+      })), React.createElement("button", {
+        onClick: this.onShippingClick
+      }, "Submit Shipping Info"));
+    } else if (this.state.level === 3) {
+      return React.createElement("form", null, React.createElement("div", null, "Payment Information:"), React.createElement("div", null, "Credit Card Number: ", React.createElement("input", {
+        type: "number",
+        placeholder: "Credit Card Number",
+        name: "cardnumber"
+      })), React.createElement("div", null, "Expiration Date: ", React.createElement("input", {
+        type: "date",
+        placeholder: "Exp Date",
+        name: "expdate"
+      })), React.createElement("div", null, "CVV: ", React.createElement("input", {
+        type: "number",
+        placeholder: "CVV",
+        name: "cvv"
+      })), React.createElement("div", null, "Zip Code: ", React.createElement("input", {
+        type: "number",
+        placeholder: "Zip Code",
+        name: "billzip"
+      })), React.createElement("button", {
+        onClick: this.onShippingClick
+      }, "Submit Payment Info"));
+    }
+  }
+
+}
+
+ReactDOM.render(React.createElement(App, null), document.getElementById("app")); // ReactDOM.render(
+//     <h1>Hello, world!</h1>,
+//     document.getElementById('app')
+//   );
